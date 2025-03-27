cad25ae1c68a46bb50af82b86b46498251558531
Jose Valentin Abundo Hernandez <valentin.abundo@bambu-mobile.com>
2025-03-26 22:03:18 -0600

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Developer on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
